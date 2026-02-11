a4936ef88e7a576214051e68d655e4bbbe9b34f3
MarcioAnuciado <166183434+MarcioAnuciado@users.noreply.github.com>
2026-02-11 19:57:39 -0300

    primeira e sugunda atividade

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
 ﻿//Nivel 1: Variáveis e Operações Básicas
-#region Calculadora de Média Simples
+#region 1. Calculadora de Média Simples
+//Console.Write("Digite a primeira nota:");
+//Decimal nota1 = Decimal.Parse(Console.ReadLine());
+//Console.Write("Digite a segunda nota: ");
+//Decimal nota2 = Decimal.Parse(Console.ReadLine());
+//Console.Write("Digite a terceira nota:");
+//Decimal nota3 = Decimal.Parse(Console.ReadLine());
+
+//Decimal media = (nota1 + nota2 + nota3) / 3;
+
+//Console.WriteLine(media);
+
 
 #endregion
-#region Conversor de idade
+#region 2. Conversor de idade
+
+//Console.Write("Digite a sua idade: ");
+//int idade = int.Parse(Console.ReadLine());
+
+//int idadeEmDias = idade * 365;
+//Console.WriteLine("Sua idade em dias é: " + idadeEmDias);
 
 #endregion
-#region Troca de Vetores
+#region 3. Troca de Vetores
 
 #endregion
+
+//Nivel 2: Estruturas Condicionais(if, else if, else)
