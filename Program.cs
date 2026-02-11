deeb1c914abfebe27d58a922cce6d6f9e9e7c6cb
MarcioAnuciado <166183434+MarcioAnuciado@users.noreply.github.com>
2026-02-11 20:48:07 -0300

    terceira atividade do nivel 3 eprimeira atividade nivel 2

--- Program.cs
+++ Program.cs
@@ -24,6 +24,39 @@
 #endregion
 #region 3. Troca de Vetores
 
+//int a = 10;
+//Console.WriteLine("Valor de a antes da troca: " + a);
+//int b = 20;
+//Console.WriteLine("Valor de b antes da troca: " + b);
+//int temp = a;
+//a = b;
+//Console.WriteLine("Valor de a depois da troca: " + a);
+//b = temp;
+//Console.WriteLine("Valor de b depois da troca: " + b);
+
 #endregion
 
 //Nivel 2: Estruturas Condicionais(if, else if, else)
+#region Verificador de Maioridade
+
+Console.WriteLine("Digite sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade >= 18)
+{
+    Console.WriteLine("Você é maior de idade.");
+}
+else
+{
+    Console.WriteLine("Você é menor de idade.");
+}
+
+#endregion
+
+#region Classificador de Números
+
+
+#endregion
+
+#region
+#endregion
