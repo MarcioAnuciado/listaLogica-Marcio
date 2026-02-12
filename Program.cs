b572c03cbaa8dffdb3556ac47f72e911e9c4a0fe
MarcioAnuciado <166183434+MarcioAnuciado@users.noreply.github.com>
2026-02-11 21:24:57 -0300

    Questão 3 do nivel 2

--- Program.cs
+++ Program.cs
@@ -54,26 +54,44 @@
 #endregion
 
 #region Classificador de Números
-Console.Write("Digite um número inteiro: ");
-int numero = int.Parse(Console.ReadLine());
 
-if (numero % 2 == 0 && numero > 0) 
-{
-    Console.WriteLine("O numero " + numero + " é pár e positivo");
-} else if (numero % 2 == 1 && numero > 0)
-{
-    Console.WriteLine("O numero " + numero + " é impar e positivo");
-}
-else if (numero < 0)
-{
-     Console.WriteLine(numero + " é negativo");
-}
-else
-{
-     Console.WriteLine("É zero");
-}
+//Console.Write("Digite um número inteiro: ");
+//int numero = int.Parse(Console.ReadLine());
+
+//if (numero % 2 == 0 && numero > 0) 
+//{
+//    Console.WriteLine("O numero " + numero + " é pár e positivo");
+//} else if (numero % 2 == 1 && numero > 0)
+//{
+//    Console.WriteLine("O numero " + numero + " é impar e positivo");
+//}
+//else if (numero < 0)
+//{
+//     Console.WriteLine(numero + " é negativo");
+//}
+//else
+//{
+//     Console.WriteLine("É zero");
+//}
 
 #endregion
 
-#region
+#region Validação de login Simples
+
+string usuarioLogin = "Admin";
+int usuarioPassword = 1234;
+
+Console.Write("Digite seu nome de usário: ");
+String usuarioInput = Console.ReadLine();
+Console.Write("Digite seu senha: ");
+int passwordInput = int.Parse(Console.ReadLine());
+
+if (usuarioInput == usuarioLogin && passwordInput == usuarioPassword)
+{
+    Console.WriteLine("Login bem-sucedido! Bem-vindo, " + usuarioInput + "!");
+}
+else
+{
+    Console.WriteLine("Login falhou! Nome de usuário ou senha incorretos.");
+}
 #endregion
